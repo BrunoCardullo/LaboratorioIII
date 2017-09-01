6d292bca64b2e4937911f59279802bc2726ab04c
brunocardullo <brunocardullo@hotmail.com>
2017-09-01 20:37:13 -0300

    Fin primer modulo

---
+++ 2017/Segundo Cuatri/Labo 3/Clase03/EjercicioProfe/funcionalidad.js
@@ -0,0 +1,14 @@
+function Validar()
+{
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+
+    if (nombre== null || apellido ==null) 
+    {
+        alert("Falta completar campos!!!");   
+    }
+    else
+    {
+        
+    }
+}
