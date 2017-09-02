3e83334bdca3d11f67e1bf8f62166813ff7b5fef
brunocardullo <brunocardullo@hotmail.com>
2017-09-01 22:11:01 -0300

    fin clase

--- 2017/Segundo Cuatri/Labo 3/Clase03/EjercicioProfe/funcionalidad.js
+++ 2017/Segundo Cuatri/Labo 3/Clase03/EjercicioProfe/funcionalidad.js
@@ -3,12 +3,22 @@ function Validar()
     var nombre = document.getElementById("nombre").value;
     var apellido = document.getElementById("apellido").value;
 
-    if (nombre== null || apellido ==null) 
+    if (nombre== "" || apellido =="") 
     {
-        alert("Falta completar campos!!!");   
+        document.getElementById("nombre").className = "error";
+        document.getElementById("apellido").className = "error";
+        alert("Falta completar campos!!!"); 
+        return;
     }
-    else
+
+    if(confirm("Esta seguro que desea agregar una persona?")==true)
     {
-        
+        document.getElementById("nombre").className="sinError";
+        document.getElementById("apellido").className="sinError";
+        var tCuerpo = document.getElementById("tCuerpo");
+        tCuerpo.innerHTML = tCuerpo.innerHTML +
+        "<tr><td>" + nombre + "</td>"+
+        "<td>" + apellido + "</td>"+
+        "<td><a href=''>borrar</a></td></tr>";
     }
 }
