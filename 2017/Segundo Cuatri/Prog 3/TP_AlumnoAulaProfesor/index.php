b4756caa3a1b1881b1147aab1b55596753fcb44c
BrunoCardullo <brunocardullo@hotmail.com>
2017-09-03 23:43:26 -0300

    fin dia

--- 2017/Segundo Cuatri/Prog 3/TP_AlumnoAulaProfesor/index.php
+++ 2017/Segundo Cuatri/Prog 3/TP_AlumnoAulaProfesor/index.php
@@ -5,9 +5,10 @@ include_once "./Clases/Alumno.php";
 include_once "./Clases/IMostrarPersonas.php";
 include_once "./Clases/Profesor.php";
 
-$AlumnoUno = new Alumno("Pedro", "Alfonso", "Masculino", "102731");
-$AlumnoDos = new Alumno("Carlos", "Alberto", "Masculino", "145566");
+$alumnoUno = new Alumno("Pedro", "Alfonso", "Masculino", "102731");
+$alumnoDos = new Alumno("Carlos", "Alberto", "Masculino", "145566");
 
 aula1 = new Aula();
 $aula1->AgregarAlumno($AlumnoUno);
-$aula1->MostrarPersonas();
+$aula1->MostrarPersona($AlumnoUno);
+$alumnoUno->MostrarDatos();
